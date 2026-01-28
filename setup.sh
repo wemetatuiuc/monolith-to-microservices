9658974e25904ab894dd18e98f09aeb8912e868b
Mian Zhang <wetmetatuiuc@gmail.com>
2026-01-28 20:31:12 +0800

    shell replacements

--- setup.sh
+++ setup.sh
@@ -21,16 +21,6 @@ if [ -z "$CLOUD_SHELL" ]; then
 
   npm install -g npm > /dev/null 2>&1
   printf "Completed.\n\n"
-
-  printf "Setting up NVM...\n"
-  export NVM_DIR="/usr/local/nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  printf "Completed.\n\n"
-  
-  printf "Updating nodeJS version...\n"
-  nvm install --lts
-  printf "Completed.\n\n"
 fi
 
 printf "Installing monolith dependencies...\n"
